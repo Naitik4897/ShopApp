da183b68e5d1a0504b8f2a32f412b12de9368f33
Naitik4897 <naitikmaisuriya227@gmail.com>
2025-07-18 08:38:38 +0530

    Add Netlify configuration and fix responsive grid layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,14 +7,12 @@ const Home = () => {
     const [loading, setLoading] = useState(false);
     const [posts, setPosts] = useState([]);
 
-
     async function fetchProductData() {
         setLoading(true);
         try {
             const res = await fetch(API_URL);
             const data = await res.json();
             console.log(data);
-
             setPosts(data);
         } catch(error) {
             console.error("Error fetching product data:", error);
@@ -31,7 +29,7 @@ const Home = () => {
             {
                 loading ? <Spinner /> :
                     posts.length > 0 ? (
-                    <div className='grid grid-cols-4 max-w-6xl p-2 mx-auto space-y-10 space-x-5 min-h-[80vh]'>
+                    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-6xl p-2 mx-auto gap-5 min-h-[80vh]'>
                         {
                             posts.map((post) => (
                                 <Product key = {post.id} post={post}/>
